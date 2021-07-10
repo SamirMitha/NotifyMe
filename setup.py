9963acf6d307ef25b610d5a6c0b830800965d809
Samir Mitha <samir.mitha@gmail.com>
2021-07-10 00:38:57 -0400

    update

--- setup.py
+++ setup.py
@@ -7,12 +7,13 @@ with open(path.join(this_directory, 'pypi.md'), encoding='utf-8') as f:
 
 setuptools.setup(
     name="notifyme_traveltimer",
-    version="1.0.1",
+    version="1.0.2",
     license="MIT",
     author="Samir Mitha, Dumitru Cotorobai",
     author_email="samir.mitha@gmail.ca",
     description="Functions for use with end to end travel and wait timing.",
-    long_descriptio=long_description,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url = "https://github.com/SamirMitha/NotifyMe",
     packages=setuptools.find_packages(),
     install_requires = [

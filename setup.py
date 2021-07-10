7f0a495c93ebac69c6391b3e7b4e9421c9f12d02
Samir Mitha <samir.mitha@gmail.com>
2021-07-09 22:04:32 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 import setuptools
 
 setuptools.setup(
-    name="notifyme-api",
-    version="0.0.1",
+    name="notifyme",
+    version="1.0.0",
+    license="MIT",
     author="Samir Mitha, Dumitru Cotorobai",
-    author_email="samir.mitha@ryerson.ca",
+    author_email="samir.mitha@gmail.ca",
     description="Functions for use real-time end to end travel and wait timing.",
+    url = "https://github.com/SamirMitha/NotifyMe"
     packages=setuptools.find_packages(),
+    install_requires = [
+        'googlemaps',
+        'pyowm',
+        'tweepy',
+        'pandas'
+    ],
     python_requires=">=3.8",
 )

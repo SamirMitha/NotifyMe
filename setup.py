1311d2e7af7d47037b886c4d020d0b0ed6bd60ca
Samir Mitha <samir.mitha@gmail.com>
2021-07-09 23:25:40 -0400

    updated

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'pypi.md'), encoding='utf-8') as f:
 
 setuptools.setup(
     name="notifyme_traveltimer",
-    version="1.0.0",
+    version="1.0.1",
     license="MIT",
     author="Samir Mitha, Dumitru Cotorobai",
     author_email="samir.mitha@gmail.ca",
